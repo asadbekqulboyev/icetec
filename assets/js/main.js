2fce2e7c53b7e54de74d215765d6eebe7f82fe44
asadbek <asadbekqulboyev@gmail.com>
2025-12-21 11:04:39 +0500

    add catalog

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,44 @@ const only_mobile = document.querySelector(".only_mobile");
 only_mobile.addEventListener("click", function () {
   document.querySelector(".search_input").classList.toggle("active");
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const sort_content = document.querySelector(".sort_content");
+  const sort_top = sort_content.querySelector(".sort_top");
+  const sort_input = sort_content.querySelector("input");
+  const sort_options = sort_content.querySelector(".sort_options");
+  const option_links = sort_options.querySelectorAll("a");
+
+  // TOP bosilganda ochish / yopish
+  sort_top.addEventListener("click", function () {
+    sort_options.classList.toggle("active");
+    sort_top.classList.toggle("active");
+  });
+
+  // OPTION bosilganda
+  option_links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const text = this.querySelector("span").innerText;
+      sort_input.value = text;
+
+      // yopish
+      sort_options.classList.remove("active");
+      sort_top.classList.remove("active");
+    });
+  });
+
+  // tashqariga bosilganda yopish (optional, professional)
+  document.addEventListener("click", function (e) {
+    if (!sort_content.contains(e.target)) {
+      sort_options.classList.remove("active");
+      sort_top.classList.remove("active");
+    }
+  });
+});
+document.addEventListener("click", function (e) {
+  const like = e.target.closest(".like");
+  if (!like) return;
+
+  like.classList.toggle("active");
+});
