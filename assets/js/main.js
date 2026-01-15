383f2eae7bdbb4adf687813ffcb221337cc21768
asadbek <asadbekqulboyev@gmail.com>
2026-01-15 16:28:58 +0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,7 +201,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
 const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
   this.classList.add("active");
